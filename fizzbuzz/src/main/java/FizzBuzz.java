f918523f2f061014a493e478ed0e53726b991bd6
chatupon khooha <khochatupon@gmail.com>
2017-07-20 11:18:26 +0700

    Complete test case 4 Fizzbuzz

--- fizzbuzz/src/main/java/FizzBuzz.java
+++ fizzbuzz/src/main/java/FizzBuzz.java
@@ -1,9 +1,7 @@
 public class FizzBuzz {
     public String say(int input) {
-        if (input <= 2)
-            return Integer.toString(input);
-        else if (input == 3)
+        if (input == 3)
             return "Fizz";
-        return null;
+        return Integer.toString(input);
     }
 }
