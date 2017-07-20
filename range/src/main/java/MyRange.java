bc79d4343cb335c7ec6cb1839851235a7920b149
chatupon khooha <khochatupon@gmail.com>
2017-07-20 19:09:28 +0700

    Complete test case 1 of range

--- range/src/main/java/MyRange.java
+++ range/src/main/java/MyRange.java
@@ -1,21 +1,29 @@
 public class MyRange {
     public String of(String input) {
+
         return null;
     }
 
     public int getLastNumber(String input) {
+
         return 0;
     }
 
     public boolean isก้ามปูปิด(String input) {
+
         return false;
     }
 
     public int getFirstNumber(String input) {
+
         return 0;
     }
 
     public boolean isก้ามปูเปิด(String input) {
+
+        if(input.charAt(0)=='[')
+            return true;
+
         return false;
     }
 }
