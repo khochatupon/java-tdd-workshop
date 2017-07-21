e75fd89a9be7c0feca016bdbd144d93492775fe3
chatupon khooha <khochatupon@gmail.com>
2017-07-21 14:22:17 +0700

    Complete test case 1 of Format Phone Number

--- format_phone_number/src/main/java/FormatPhoneNumber.java
+++ format_phone_number/src/main/java/FormatPhoneNumber.java
@@ -1,10 +1,18 @@
 public class FormatPhoneNumber {
 
     public String removeNonDigit(String input) {
-        return null;
+        String strNumber = "";
+        for (int i=0;i<input.length();i++) {
+            int num = Character.getNumericValue(input.charAt(i));
+            if (num >= 0) {
+                strNumber += num;
+            }
+        }
+        return strNumber;
     }
 
     public String solution(String input) {
+
         return null;
     }
 
